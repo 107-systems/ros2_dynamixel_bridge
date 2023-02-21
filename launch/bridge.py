a4a74db4bdd6d6dc9325939f82d2ffb9915e849d
Alexander Entinger <consulting@lxrobotics.com>
2023-02-21 11:27:42 +0100

    Remape topic names towards the ones needed by L3XZ control software.

--- launch/bridge.py
+++ launch/bridge.py
@@ -13,46 +13,14 @@ def generate_launch_description():
       parameters=[
           {'serial_port' : '/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT4NNZ55-if00-port0'},
           {'serial_port_baudrate': 2*1000*1000},
-
-          {'left_front_coxa_servo_id': 1},
-          {'left_front_coxa_servo_initial_angle': 180.0},
-          {'left_front_coxa_servo_min_angle': 180.0 - 35.0},
-          {'left_front_coxa_servo_max_angle': 180.0 + 35.0},
-
-          {'left_middle_coxa_servo_id': 2},
-          {'left_middle_coxa_servo_initial_angle': 180.0},
-          {'left_middle_coxa_servo_min_angle': 180.0 - 35.0},
-          {'left_middle_coxa_servo_max_angle': 180.0 + 35.0},
-
-          {'left_back_coxa_servo_id': 3},
-          {'left_back_coxa_servo_initial_angle': 180.0},
-          {'left_back_coxa_servo_min_angle': 180.0 - 35.0},
-          {'left_back_coxa_servo_max_angle': 180.0 + 35.0},
-
-          {'right_back_coxa_servo_id': 4},
-          {'right_back_coxa_servo_initial_angle': 180.0},
-          {'right_back_coxa_servo_min_angle': 180.0 - 35.0},
-          {'right_back_coxa_servo_max_angle': 180.0 + 35.0},
-
-          {'right_middle_coxa_servo_id': 5},
-          {'right_middle_coxa_servo_initial_angle': 180.0},
-          {'right_middle_coxa_servo_min_angle': 180.0 - 35.0},
-          {'right_middle_coxa_servo_max_angle': 180.0 + 35.0},
-
-          {'right_front_coxa_servo_id': 6},
-          {'right_front_coxa_servo_initial_angle': 180.0},
-          {'right_front_coxa_servo_min_angle': 180.0 - 35.0},
-          {'right_front_coxa_servo_max_angle': 180.0 + 35.0},
-
-          {'pan_servo_id': 7},
-          {'pan_servo_initial_angle': 180.0},
-          {'pan_servo_min_angle': 180.0 - 35.0},
-          {'pan_servo_max_angle': 180.0 + 35.0},
-
-          {'tilt_servo_id': 8},
-          {'tilt_servo_initial_angle': 180.0},
-          {'tilt_servo_min_angle': 180.0 - 35.0},
-          {'tilt_servo_max_angle': 180.0 + 35.0},
+      ],
+      remappings=[
+          ('/l3xz/dynamixel/servo_7/angle/actual',            '/l3xz/head/pan/angle/actual'),
+          ('/l3xz/dynamixel/servo_7/angle/target',            '/l3xz/head/pan/angle/target'),
+          ('/l3xz/dynamixel/servo_7/angular_velocity/target', '/l3xz/head/pan/angular_velocity/target'),
+          ('/l3xz/dynamixel/servo_8/angle/actual',            '/l3xz/head/tilt/angle/actual'),
+          ('/l3xz/dynamixel/servo_8/angle/target',            '/l3xz/head/tilt/angle/target'),
+          ('/l3xz/dynamixel/servo_8/angular_velocity/target', '/l3xz/head/tilt/angular_velocity/target'),
       ]
     )
   ])
