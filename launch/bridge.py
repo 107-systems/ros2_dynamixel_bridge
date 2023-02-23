27680b50bc2dc7c04f75a7507ac343235708a76f
Alexander Entinger <consulting@lxrobotics.com>
2023-02-23 10:08:03 +0100

    Topic remappings need to be done here (for now).

--- launch/bridge.py
+++ launch/bridge.py
@@ -16,5 +16,15 @@ def generate_launch_description():
           {'required_node_id_list': [1, 2, 3, 4, 5, 6, 7, 8]},
           {'check_required_node_id_list': True},
       ],
+      remappings=[
+          ('/dynamixel/servo_7/angle/actual',            '/l3xz/head/pan/angle/actual'),
+          ('/dynamixel/servo_7/angle/target',            '/l3xz/head/pan/angle/target'),
+          ('/dynamixel/servo_7/angular_velocity/target', '/l3xz/head/pan/angular_velocity/target'),
+          ('/dynamixel/servo_7/mode/set',                '/l3xz/head/pan/mode/set'),
+          ('/dynamixel/servo_8/angle/actual',            '/l3xz/head/tilt/angle/actual'),
+          ('/dynamixel/servo_8/angle/target',            '/l3xz/head/tilt/angle/target'),
+          ('/dynamixel/servo_8/angular_velocity/target', '/l3xz/head/tilt/angular_velocity/target'),
+          ('/dynamixel/servo_8/mode/set',                '/l3xz/head/tilt/mode/set'),
+      ],
     )
   ])
